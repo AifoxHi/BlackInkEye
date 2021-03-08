b2418e40f18e33a8b8e9bcdd0d9954a120199824
Sofia (lolzzz992) <lolzzz992q@gmail.com>
2021-03-09 00:43:50 +0300

    Don't Worry

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,21 @@ import router from "./router";
 import store from "./store";
 import VueFinalModal from 'vue-final-modal';
 import { createI18n } from 'vue-i18n';
+import 'vue-i18n';
 import eng from './locales/en.json'
 import rus from './locales/ru.json'
 import jpn from './locales/ja.json'
 import deu from './locales/de.json'
 import fra from './locales/fr.json'
-import './assets/scrollreveal.min.js'
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import VueMarkdownIt from 'vue3-markdown-it';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPhone, faCoffee, faGlobeAmericas  } from '@fortawesome/free-solid-svg-icons'
+import { faFontAwesome, faJs, faVuejs } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faFontAwesome, faPhone, faCoffee, faGlobeAmericas, faJs, faVuejs)
 
 const i18n = createI18n({
   messages: {
@@ -20,13 +29,15 @@ const i18n = createI18n({
     de: deu,
     fr: fra
 },
-    locale: window.localStorage.Language || 'en',
-    fallbackLocale: 'ru'
+    locale: window.localStorage.Language || window.localStorage.sessionStorage || window.navigator.language, 
+    fallbackLocale: 'en'
 })
-
+    
 createApp(App)
   .use(store)
   .use(router)
   .use(i18n)
+  .use(VueMarkdownIt)
   .use(VueFinalModal())
+  .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
